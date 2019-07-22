ed898bec46e7d20f7b06b520c74b3123196ccf7b
John T. Foster <johntfosterjr@gmail.com>
2019-07-22 13:36:52 -0500

    Fixed bug (#2)

--- file.c
+++ file.c
@@ -1,5 +1,5 @@
-#include stdio.h
+#include "stdio.h"
 #include <errno.h>
-int quit(char *message) {
-	printf('Error number %10d', errno)
-        exit;
+void quit(char *message) {
+	printf('Error number %d', errno)
+        exit(1);
